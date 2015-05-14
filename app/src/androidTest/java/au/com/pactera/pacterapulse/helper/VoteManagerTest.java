fefe5ceaf534a8243cbe3b063d05f980261a2404
Yanqing Lin <yanqing.lin@news.com.au>
2015-05-14 14:59:09 +1000

    PACOS-57: add votemanager test class

---
+++ app/src/androidTest/java/au/com/pactera/pacterapulse/helper/VoteManagerTest.java
@@ -0,0 +1,16 @@
+package au.com.pactera.pacterapulse.helper;
+
+
+/**
+ * Created by liny1 on 14/05/2015.
+ */
+public class VoteManagerTest {
+
+    public void testSubmitVote() throws Exception {
+
+    }
+
+    public void testIsVotedToday() throws Exception {
+
+    }
+}
