142178af0f9ec147c5bba20cc3e5883961b7b4ae
Yanqing Lin <yanqing.lin@news.com.au>
2015-05-14 14:57:12 +1000

    PACOS-57: create votemanager class

---
+++ app/src/main/java/au/com/pactera/pacterapulse/helper/VoteManager.java
@@ -0,0 +1,25 @@
+package au.com.pactera.pacterapulse.helper;
+
+/**
+ * Created by liny1 on 14/05/2015.
+ */
+public class VoteManager {
+
+    /**
+     * submit the vote with the emotion value
+     * @param emotion the emotion value in integer
+     */
+    public void submitVote(int emotion){
+
+    }
+
+    /**
+     * check if the user has voted the emotion today
+     * @return isVoted the boolean value
+     */
+    public boolean isVotedToday(){
+        boolean isVoted = false;
+        return isVoted;
+    }
+
+}
