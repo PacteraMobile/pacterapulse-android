ef6f19fbc7a77d248f6bd532ccf71374c18fe2f7
Kai Liao <soarcn@gmail.com>
2015-05-21 16:04:59 +1000

    disable action buttons while loader is working

--- app/src/main/java/au/com/pactera/pacterapulse/fragment/ResultFragment.java
+++ app/src/main/java/au/com/pactera/pacterapulse/fragment/ResultFragment.java
@@ -2,17 +2,24 @@ package au.com.pactera.pacterapulse.fragment;
 
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import java.util.List;
 
 import au.com.pactera.pacterapulse.R;
 import au.com.pactera.pacterapulse.chart.EmotionBarChartView;
 import au.com.pactera.pacterapulse.core.BaseFragment;
 import au.com.pactera.pacterapulse.helper.NetworkHelper;
 import au.com.pactera.pacterapulse.model.Emotions;
+import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.InjectViews;
 import butterknife.OnCheckedChanged;
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
@@ -25,6 +32,9 @@ public class ResultFragment extends BaseFragment<Emotions> {
     ProgressBar progressBar;
     @InjectView(R.id.chartView)
     EmotionBarChartView chartView;
+
+    @InjectViews({R.id.oneweek,R.id.oneday,R.id.onemonth})
+    List<View> radios;
 
     private String currentType = "24hours";
 
@@ -66,11 +76,23 @@ public class ResultFragment extends BaseFragment<Emotions> {
     @Override
     protected void onStartLoading() {
         progressBar.setVisibility(View.VISIBLE);
+        ButterKnife.apply(radios, new ButterKnife.Action<View>() {
+            @Override
+            public void apply(View view, int index) {
+                view.setEnabled(false);
+            }
+        });
     }
 
     @Override
     protected void onStopLoading() {
         progressBar.setVisibility(View.GONE);
+        ButterKnife.apply(radios, new ButterKnife.Action<View>() {
+            @Override
+            public void apply(View view, int index) {
+                view.setEnabled(true);
+            }
+        });
     }
 
     @Override
