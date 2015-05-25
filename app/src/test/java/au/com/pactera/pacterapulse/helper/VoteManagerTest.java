861e2c37ca48556ff7253ba7ff0ef0a2ffae5227
Kai Liao <soarcn@gmail.com>
2015-05-25 14:22:34 +1000

    Support Junit test for project, this is for non-ui unit test, and can be trigger by gradle  test

---
+++ app/src/test/java/au/com/pactera/pacterapulse/helper/VoteManagerTest.java
@@ -0,0 +1,22 @@
+package au.com.pactera.pacterapulse.helper;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Test;
+
+/**
+ * Created by liny1 on 14/05/2015.
+ */
+@SmallTest
+public class VoteManagerTest {
+
+    @Test
+    public void testSubmitVote() throws Exception {
+
+    }
+
+    @Test
+    public void testIsVotedToday() throws Exception {
+
+    }
+}
