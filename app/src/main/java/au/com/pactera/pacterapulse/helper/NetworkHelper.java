a908e3b506599e45e22dc74172317fd84dceb7c9
Chaniel Yu <Chaniel.Yu@pactera.com.au>
2015-05-28 11:33:30 +1000

    Add CopyRight

--- app/src/main/java/au/com/pactera/pacterapulse/helper/NetworkHelper.java
+++ app/src/main/java/au/com/pactera/pacterapulse/helper/NetworkHelper.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2015 Pactera. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED AS IS BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ * ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
+ * PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache License, Version 2.0 for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 package au.com.pactera.pacterapulse.helper;
 
 import android.content.Context;
@@ -13,11 +31,6 @@ import org.json.JSONObject;
 import au.com.pactera.pacterapulse.app.Config;
 import au.com.pactera.pacterapulse.model.Emotions;
 
-
-/**
- * Created by Lubor Kolacny on 22/04/15.
- * Copyright (c) 2015 Pactera Technology. All rights reserved.
- */
 
 public class NetworkHelper
 {
