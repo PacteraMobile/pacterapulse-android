a94148f9fc287df325529ddc4a9dfd8bcf6fdbd4
RaghuPactera <angara.raghunandan@pactera.com.au>
2015-06-01 14:00:33 +1000

    Add Template to new UnitTest Code

--- app/src/androidTest/java/au/com/pactera/pacterapulse/instrument/IntroductionFragmentTest.java
+++ app/src/androidTest/java/au/com/pactera/pacterapulse/instrument/IntroductionFragmentTest.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2015 Pactera. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED AS IS BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ * ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
+ * PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache License, Version 2.0 for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 package au.com.pactera.pacterapulse.instrument;
 
 import android.support.test.rule.ActivityTestRule;
@@ -24,6 +42,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by Raghu on 29/05/2015.
+ * This is a basic Instrumentation Test using latest JUnit4 libraries
  */
 
 @RunWith(AndroidJUnit4.class)
