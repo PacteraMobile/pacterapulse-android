fd85ac67dd77a7c1ff683bd0afe8276054d824ad
RaghuPactera <angara.raghunandan@pactera.com.au>
2015-06-11 16:20:29 +1000

    Testing Espresso Tests on Travis AVD

--- app/src/androidTest/java/au/com/pactera/pacterapulse/instrument/IntroductionFragmentJUnit3Test.java
+++ app/src/androidTest/java/au/com/pactera/pacterapulse/instrument/IntroductionFragmentJUnit3Test.java
@@ -1,13 +1,32 @@
+/*
+ * Copyright (c) 2015 Pactera. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED AS IS BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ * ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
+ * PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache License, Version 2.0 for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 package au.com.pactera.pacterapulse.instrument;
 
-import android.support.test.espresso.action.ViewActions;
-import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.espresso.matcher.RootMatchers;
 import android.test.ActivityInstrumentationTestCase2;
 
 import au.com.pactera.pacterapulse.MainActivity;
-import au.com.pactera.pacterapulse.R;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by Test User on 9/06/2015.
@@ -31,8 +50,24 @@ public class IntroductionFragmentJUnit3Test extends ActivityInstrumentationTestC
 
 
 	public void testCheckPreconditions() throws Exception {
-		onView(ViewMatchers.withId(R.id.btnHappy))
-				.perform(ViewActions.click());
+		//onView(withId(R.id.btnAgree)).perform(click());
+
+		onView(withText("Loading...")).inRoot(RootMatchers.isDialog()).check(matches(isDisplayed()));
+
+
+		//onView(withText("Loading...")).inRoot(RootMatchers.isDialog()).check(matches(isDisplayed()));
+
+		//Thread.sleep(2500);
+
+		//onView(withText("Sign in")).check(matches(isDisplayed()));
+	}
+
+	/*
+	 * You cannot test Login Page as Espresso does not support WEB VIEWS as of now!
+	 * http://stackoverflow.com/questions/29144310/match-a-view-in-webview-from-espresso-in-android
+	 */
+	public void testLoginPageDisplay() throws Exception {
+//		//onView(withText("Sign in")).check(matches(isDisplayed()));
 	}
 
 	@Override
